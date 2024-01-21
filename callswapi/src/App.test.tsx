152a20f2a18b0e9c1c5d05a01ee7cb67504b297b
smithaagarwal <smitha.kotian@gmail.com>
2024-01-21 20:50:51 +0000

    Initial commit

---
+++ callswapi/src/App.test.tsx
@@ -0,0 +1,28 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import "@testing-library/jest-dom";
+
+import App from './App';
+
+
+describe('something truthy and falsy', () => {
+  it('true to be true', () => {
+    expect(true).toBe(true);
+  });
+
+  it('false to be false', () => {
+    expect(false).toBe(false);
+  });
+});
+
+describe('App', () => {
+    it('renders headline', () => {
+      render(<App />);
+  
+      screen.debug();
+      const heading = screen.getByText("Vite + React");
+      expect(heading).toBeInTheDocument();
+  
+      // check if App components renders headline
+    });
+  });
